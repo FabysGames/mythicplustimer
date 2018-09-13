5586f77b5715bb8aba7d65ce4f6bc92a6f592942
Fabys <fabys@fabys.eu>
2018-09-13 17:36:41 +0200

    zhCN

---
+++ Locales/zhCN.lua
@@ -0,0 +1,31 @@
+-- zhCN translations by Littlebaoooo (https://wow.curseforge.com/members/Littlebaoooo)
+-- Thank You!
+
+local L = LibStub("AceLocale-3.0"):NewLocale("MythicPlusTimer", "zhCN")
+if L == nil then
+    return
+end
+
+L["ToggleCommandText"] = "锁定/解锁计时器框体"
+L["Loot"] = "战利品"
+L["NoLoot"] = "无战利品"
+L["Best"] = "最佳"
+L["ObjectiveTimes"] = "显示目标时间"
+L["ObjectiveTimesDesc"] = "显示完成时间及每个目标的最佳时间."
+L["DeleteBestTimes"] = "删除最佳时间"
+L["DeleteBestTimesRecords"] = "删除最佳时间记录."
+L["DeathCounter"] = "死亡计数器 (限制: 如果死亡过远则不计数)"
+L["DeathCounterDesc"] = "显示死亡计数器和因玩家死亡而损失的时间. (每次死亡损失5s)"
+L["Deaths"] = "死亡"
+L["Completed"] = "完成"
+L["Time"] = "时间"
+L["BestTime"] = "最佳时间"
+L["ObjectiveTimesInChat"] = "在聊天框中显示完成时间"
+L["ObjectiveTimesInChatDesc"] = "以聊天信息形式显示完成时间."
+L["TimeLeft"] = "剩余时间"
+L["ObjectiveTimePerLevel"] = "每级完成时间"
+L["ObjectiveTimePerLevelDesc"] = "显示每级完成时间，而不是整个大秘境完成时间."
+L["ProgressTooltip"] = "在鼠标提示中显示进度百分比 (通过击杀大秘境怪物来建立数据库)"
+L["ProgressTooltipDesc"] = "在鼠标提示中显示怪物进度."
+L["DeleteNPCProgress"] = "删除进度百分比数据"
+L["DeleteNPCProgressDesc"] = "清除进度百分比数据库."
